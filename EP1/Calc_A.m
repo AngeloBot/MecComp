1f4dacad1ae46b4e6f09ad02779c50f1b8d3c072
AngeloBot <33585124+AngeloBot@users.noreply.github.com>
2020-03-10 21:20:24 -0300

    implementação incompleta do método de euler

--- EP1/Calc_A.m
+++ EP1/Calc_A.m
@@ -1,10 +1,9 @@
 function [A,A0,A5] = Calc_A()
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
-
+global th1 th2 L1 L2 L2e m1 m2 F1 F2 uIz R Vel;
 A=[0 (L1^2)*L2*R*m2*sin(2*th1-2*th2) 2*L1*(L2^2)*R*m2*sin(th1-th2) -2*L2*uIz*Vel -2*L1*uIz*Vel*cos(th1-th2)];
 A0=(L1^2)*L2*R*(m2*cos(2*th1-2*th2)-2*m1-m2);
 A5=-R*L1*(L2e*F2*sin(th1-2*th2)+2*sin(th1)*(F1*L2+0.5*L2e*F2));
-
 end
 
--- EP1/EP1_AYRA.m
+++ EP1/EP1_AYRA.m
@@ -3,26 +3,11 @@
 %Ângelo Bianco Yanagita 6649978
 %Rodrigo Heira Akamine NUSP
 
+clear all;
+close all;
+clc;
+global L1 L2 L2e m1 m2 g F1 F2 uIz R dx_d Vel;
 %Definição de Parâmetros e Constantes:
-global th1;
-global th2;
-global dth1;
-global dth2;
-global ddth1;
-global ddth2;
-
-global L1;
-global L2;
-global L2e;
-global m1;
-global m2;
-global g;
-global F1;
-global F2;
-global uIz;
-global R;
-global dx_d;
-global Vel;
 
 L1=2;%m
 L2=2.5;%m
@@ -37,6 +22,47 @@ R=0.3;%m
 dx_d=80;%km/h
 Vel=dx_d;%km/h
 
-EulerMethod(t_i,t_f,h,Y_0);
+t_i=input('Tempo inicial(s) ','s');
+t_f=input('Tempo final(s) ','s');
+h_max=input('Passo máximo(s) ','s');
+h_min=input('Passo mínimo(s) ','s');
+h_var=input('número de incrementos de passo ','s');
+Y_0=input('vetor de CI(SI)[dth1,dth2,th1,th2] ','s');
 
+t_i = str2num(['uint8(',t_i,')']);
+t_f= str2num(['uint8(',t_f,')']);
+h_max= str2num(['uint8(',h_max,')']);
+h_min= str2num(['uint8(',h_min,')']);
+h_var= str2num(['uint8(',h_var,')']);
+Y_0=str2num(Y_0);
+
+which_run=input('Qual método rodar: Euler(E) ','s'); 
+
+if which_run=='E'
+    
+    figure(1);
+    
+    t=t_i:h_min:t_f;
+    E={EulerMethod(t_i,t_f,h_min,Y_0), num2str(h_min), t};
+    a=E(1);
+    whos a
+    plot(t,E(1));
+    hold on;
+    
+    for h=linspace(h_min,h_max,h_var)
+        
+        t=t_i:h:t_f;
+        result_vect={EulerMethod(t_i,t_f,h,Y_0), num2str(h), t};
+        plot(t,result_vect(1));
+        
+        E=cat(2,E,result_vect);
+    end
+    
+    xlabel('Tempo(s)');
+    ylabe('Ângulo(s)');
+    legend(E(2,:));
+    title('Método de Euler');
+    
+    hold off;
+end
 
